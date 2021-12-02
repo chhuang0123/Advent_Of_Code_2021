7592ef15a5453ec67e95784131f5c948e6be71e6
Eric C Huang <eric_c_huang@trendmicro.com>
2021-12-02 15:08:23 +0800

    finish day02 adv

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -17,6 +17,22 @@ fn main() {
     }
 
     println!("{} * {} = {}", h_v.0, h_v.1, h_v.0 * h_v.1);
+
+    // part 2
+    let mut h_v_d = (0, 0, 0);
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    for (_, line) in reader.lines().enumerate() {
+        let result = parse_h_v_d(&line.unwrap(), h_v_d.1);
+
+        h_v_d.0 += result.0;
+        h_v_d.1 += result.1;
+        if result.2 > 0 {
+            h_v_d.2 += result.2;
+        }
+    }
+
+    println!("{} * {} = {}", h_v_d.0, h_v_d.2, h_v_d.0 * h_v_d.2);
 }
 
 fn parse_h_v(line: &str) -> (i32, i32) {
@@ -27,5 +43,20 @@ fn parse_h_v(line: &str) -> (i32, i32) {
         return (0, direction_value[1].parse::<i32>().unwrap());
     } else {
         return (0, direction_value[1].parse::<i32>().unwrap() * -1);
+    }
+}
+
+fn parse_h_v_d(line: &str, v: i32) -> (i32, i32, i32) {
+    let direction_value: Vec<&str> = line.split(' ').collect();
+    if direction_value[0] == "forward" {
+        return (
+            direction_value[1].parse::<i32>().unwrap(),
+            0,
+            direction_value[1].parse::<i32>().unwrap() * v,
+        );
+    } else if direction_value[0] == "down" {
+        return (0, direction_value[1].parse::<i32>().unwrap(), 0);
+    } else {
+        return (0, direction_value[1].parse::<i32>().unwrap() * -1, 0);
     }
 }
