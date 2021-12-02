489c66ef30fa28dff395bccc2eb6d87abd9f1ddd
Eric C Huang <eric_c_huang@trendmicro.com>
2021-12-02 21:06:13 +0800

    rename

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -8,22 +8,22 @@ fn main() {
     let reader = BufReader::new(file);
 
     // part 1 + 2
-    let mut h_v_d = (0, 0, 0);
+    let mut h_v_a = (0, 0, 0);
     for (_, line) in reader.lines().enumerate() {
-        let result = parse_h_v_d(&line.unwrap(), h_v_d.1);
+        let result = parse_h_v_a(&line.unwrap(), h_v_a.1);
 
-        h_v_d.0 += result.0;
-        h_v_d.1 += result.1;
+        h_v_a.0 += result.0;
+        h_v_a.1 += result.1;
         if result.2 > 0 {
-            h_v_d.2 += result.2;
+            h_v_a.2 += result.2;
         }
     }
 
-    println!("part 1: {} * {} = {}", h_v_d.0, h_v_d.1, h_v_d.0 * h_v_d.1);
-    println!("part 2: {} * {} = {}", h_v_d.0, h_v_d.2, h_v_d.0 * h_v_d.2);
+    println!("part 1: {} * {} = {}", h_v_a.0, h_v_a.1, h_v_a.0 * h_v_a.1);
+    println!("part 2: {} * {} = {}", h_v_a.0, h_v_a.2, h_v_a.0 * h_v_a.2);
 }
 
-fn parse_h_v_d(line: &str, v: i32) -> (i32, i32, i32) {
+fn parse_h_v_a(line: &str, v: i32) -> (i32, i32, i32) {
     let direction_value: Vec<&str> = line.split(' ').collect();
     let direction = direction_value[0];
     let value = direction_value[1].parse::<i32>().unwrap();
