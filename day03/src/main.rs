9d1384ab237c9c158cf8e7cd00fe1996b619e282
Eric C Huang <eric_c_huang@trendmicro.com>
2021-12-03 18:57:23 +0800

    day03 part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,6 +1,8 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+#[allow(unused_mut)]
+#[allow(unused_variables)]
 fn main() {
     let args: Vec<String> = std::env::args().skip(1).collect();
     let filename = &args[0];
@@ -13,11 +15,17 @@ fn main() {
         lines.push(line.unwrap());
     }
 
-    let (gamma, epsilon) = calculate_gamma_epsilon(&lines);
+    let zero_one_count: Vec<(i32, i32)> = get_zero_one_count(&lines);
+    // println!("{:?}", zero_one_count);
+
+    let (gamma, epsilon) = calculate_gamma_epsilon(&zero_one_count);
     println!("part 1: {} * {} = {}", gamma, epsilon, (gamma * epsilon));
+
+    // part 2
+    let oxygen = calculate_oxygen_co2(&lines);
 }
 
-fn calculate_gamma_epsilon(lines: &Vec<String>) -> (i32, i32) {
+fn get_zero_one_count(lines: &Vec<String>) -> Vec<(i32, i32)> {
     // println!("{:?}", lines);
 
     let _row_count = lines.len();
@@ -39,7 +47,13 @@ fn calculate_gamma_epsilon(lines: &Vec<String>) -> (i32, i32) {
         // println!("{:?}", zero_one_count);
     }
 
-    let (gamma_str, epsilon_str) = get_gamma_epsilon_string(&zero_one_count);
+    zero_one_count
+}
+
+fn calculate_gamma_epsilon(zero_one_count: &Vec<(i32, i32)>) -> (i32, i32) {
+    let (gamma_str, epsilon_str) = get_gamma_epsilon_string(zero_one_count);
+    // println!("{:?} {:?}", gamma_str, epsilon_str);
+
     let gamma = i32::from_str_radix(&gamma_str, 2).unwrap();
     let epsilon = i32::from_str_radix(&epsilon_str, 2).unwrap();
     // println!("{:?} {:?}", gamma, epsilon);
@@ -62,4 +76,91 @@ fn get_gamma_epsilon_string(zero_one_count: &Vec<(i32, i32)>) -> (String, String
     }
 
     (gamma.into_iter().collect(), epsilon.into_iter().collect())
+}
+
+fn calculate_oxygen_co2(lines: &Vec<String>) {
+    let oxygen_str = get_oxygen_string(&lines);
+    let oxygen = i32::from_str_radix(&oxygen_str, 2).unwrap();
+
+    let co2_str = get_co2_string(&lines);
+    let co2 = i32::from_str_radix(&co2_str, 2).unwrap();
+
+    println!("part 2: {} * {} = {}", oxygen, co2, oxygen * co2);
+}
+
+fn get_oxygen_string(lines: &Vec<String>) -> String {
+    let mut zero_one_count = get_zero_one_count(&lines);
+    let mut result: Vec<String> = Vec::new();
+    for i in 0..zero_one_count.len() {
+        // println!(
+        //     "i: {}, 0: {}, 1: {} ",
+        //     i, zero_one_count[i].0, zero_one_count[i].1
+        // );
+
+        let target_char: char;
+        if zero_one_count[i].1 >= zero_one_count[i].0 {
+            target_char = '1';
+        } else {
+            target_char = '0';
+        }
+
+        if result.len() == 0 {
+            result = get_match_string(&lines, i, target_char);
+        } else {
+            result = get_match_string(&result, i, target_char);
+        }
+        zero_one_count = get_zero_one_count(&result);
+
+        if result.len() == 1 {
+            break;
+        }
+    }
+
+    result[0].clone()
+}
+
+fn get_match_string(lines: &Vec<String>, index: usize, target_char: char) -> Vec<String> {
+    // println!("index {:?}", index);
+    // println!("target_char {:?}", target_char);
+
+    let mut result: Vec<String> = Vec::new();
+    for i in 0..lines.len() {
+        if target_char == (&*lines[i]).chars().nth(index).unwrap() {
+            result.push(lines[i].clone());
+        }
+    }
+    // println!("match result: {:?}", result);
+
+    result
+}
+
+fn get_co2_string(lines: &Vec<String>) -> String {
+    let mut zero_one_count = get_zero_one_count(&lines);
+    let mut result: Vec<String> = Vec::new();
+    for i in 0..zero_one_count.len() {
+        // println!(
+        //     "i: {}, 0: {}, 1: {} ",
+        //     i, zero_one_count[i].0, zero_one_count[i].1
+        // );
+
+        let target_char: char;
+        if zero_one_count[i].0 <= zero_one_count[i].1 {
+            target_char = '0';
+        } else {
+            target_char = '1';
+        }
+
+        if result.len() == 0 {
+            result = get_match_string(&lines, i, target_char);
+        } else {
+            result = get_match_string(&result, i, target_char);
+        }
+        zero_one_count = get_zero_one_count(&result);
+
+        if result.len() == 1 {
+            break;
+        }
+    }
+
+    result[0].clone()
 }
