85ee613b0127d30254dd2304305127207b49c31c
Eric C Huang <eric_c_huang@trendmicro.com>
2021-12-06 14:09:28 +0800

    day06 part 1

---
+++ day06/src/main.rs
@@ -0,0 +1,47 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    let filename = &args[0];
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    // part 1
+    let mut fish_list: Vec<i32> = Vec::new();
+    for line in reader.lines() {
+        fish_list = line
+            .unwrap()
+            .split(',')
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect();
+    }
+
+    after_timer(80, &mut fish_list);
+    println!("part 1: {}", fish_list.len());
+}
+
+fn after_timer(days: i32, fish_list: &mut Vec<i32>) {
+    // println!("Initial state: {:?}", fish_list);
+
+    for i in 0..days {
+        let zero_count = fish_list.iter().filter(|x| **x == 0).count();
+        // println!("zero: {}", zero_count);
+
+        for j in 0..fish_list.len() {
+            let tmp_time = if fish_list[j as usize] - 1 == -1 {
+                6
+            } else {
+                fish_list[j as usize] - 1
+            };
+
+            fish_list[j as usize] = tmp_time;
+        }
+
+        for _ in 0..zero_count {
+            fish_list.push(8);
+        }
+
+        // println!("After {:-2} day: {:?}", i + 1, fish_list);
+    }
+}
