77a7ea38a09cfe8cbd5e5703c1133d0afff4e6e4
Eric C Huang <eric_c_huang@trendmicro.com>
2021-12-13 14:02:46 +0800

    day12 part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,5 +1,5 @@
 use log::{debug, info};
-use std::collections::{BTreeMap};
+use std::collections::BTreeMap;
 use std::env;
 use std::fs;
 
@@ -35,6 +35,12 @@ fn main() {
 
     debug!("all_path {:?}", all_path);
     info!("part 1: {}", all_path.len());
+
+    let mut visited: Vec<String> = Vec::new();
+    let mut all_path: Vec<Vec<String>> = Vec::new();
+    find_all_path_2(&graph, "start".to_string(), &mut visited, &mut all_path);
+    debug!("all_path {:?}", all_path);
+    info!("part 2: {}", all_path.len());
 }
 
 fn find_all_path(
@@ -57,6 +63,53 @@ fn find_all_path(
             }
 
             find_all_path(
+                &graph,
+                node.clone().to_string(),
+                &mut visited.clone(),
+                all_path,
+            );
+        }
+    }
+}
+
+fn find_all_path_2(
+    graph: &BTreeMap<String, Vec<String>>,
+    from: String,
+    visited: &mut Vec<String>,
+    all_path: &mut Vec<Vec<String>>,
+) {
+    visited.push(from.clone());
+    debug!("visited {:?}", visited);
+    if from == "end" {
+        all_path.push(visited.clone());
+    }
+
+    for nodes in graph.get(&from) {
+        for node in nodes {
+            let only_once = node.chars().nth(0).unwrap().is_lowercase();
+            let is_contained = visited.contains(node);
+
+            let frequencies = visited
+                .iter()
+                .filter(|x| {
+                    *x != "start" && *x != "end" && x.chars().nth(0).unwrap().is_lowercase()
+                })
+                .fold(BTreeMap::new(), |mut freqs, value| {
+                    *freqs.entry(value).or_insert(0) += 1;
+                    freqs
+                });
+            debug!("{:?}", frequencies);
+
+            let seen_length_check = frequencies.iter().filter(|&(_, v)| *v >= 2).count() >= 1;
+            if only_once && is_contained && seen_length_check {
+                continue;
+            }
+
+            if (node == "start" || node == "end") && visited.contains(node) {
+                continue;
+            }
+
+            find_all_path_2(
                 &graph,
                 node.clone().to_string(),
                 &mut visited.clone(),
