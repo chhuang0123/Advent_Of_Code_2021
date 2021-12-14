804c0516261f1c7c5db44e9feb8fd1b8a75340f3
Eric C Huang <eric_c_huang@trendmicro.com>
2021-12-14 11:20:23 +0800

    day13 part 1

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -6,7 +6,7 @@ use std::fs;
 #[derive(Debug)]
 struct Point(usize, usize);
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Fold(char, usize);
 
 fn main() {
@@ -34,7 +34,7 @@ fn main() {
             debug!("x_max {:?} y_max {:?}", x_max, y_max);
 
             debug!("x {:?} y {:?}", x, y);
-            point_list.push(Point(x, y));
+            point_list.push(Point(y, x));
         } else if line.contains("=") {
             let re = Regex::new(r"([xy])=(\d+)").unwrap();
             for cap in re.captures_iter(&line) {
@@ -55,20 +55,65 @@ fn main() {
 
     let mut point_matrix = vec![vec!['.'; x_max + 1]; y_max + 1];
     debug!("point_matrix {:?}", point_matrix);
+
     fill(&point_list, &mut point_matrix);
 
-    fold(&fold_list, &mut point_matrix)
+    let mut min_point = Point(0, 0);
+    for (index, fold_axis) in fold_list.iter().enumerate() {
+        debug!("index {:?} fold_axis {:?}", index, fold_axis);
+
+        // print_all(&point_matrix);
+        debug!("x_min {:?} y_min {:?}", min_point.0, min_point.1);
+        fold(&mut min_point, *fold_axis, &mut point_matrix);
+        debug!("x_min {:?} y_min {:?}", min_point.0, min_point.1);
+        // print_all(&point_matrix);
+
+        if index == 0 {
+            let result = print_word(min_point.0, min_point.1, &point_matrix);
+            info!("part 1: {}", result);
+        }
+    }
+
+    // info!("x_min {:?} y_min {:?}", min_point.0, min_point.1);
+    // print_word(min_point.0, min_point.1, &point_matrix);
+}
+
+fn print_word(x_max: usize, y_max: usize, point_matrix: &Vec<Vec<char>>) -> usize {
+    let x_max = if x_max == 0 {
+        point_matrix[0].len()
+    } else {
+        x_max
+    };
+    let y_max = if y_max == 0 {
+        point_matrix.len()
+    } else {
+        y_max
+    };
+
+    let mut count = 0;
+    for i in 0..y_max {
+        for j in 0..x_max {
+            print!("{}", point_matrix[i][j]);
+            if point_matrix[i][j] == '#' {
+                count += 1;
+            }
+        }
+        println!("");
+    }
+
+    count
 }
 
 fn get_point(point_matrix: &Vec<Vec<char>>) -> Vec<Point> {
-    let y_max = point_matrix.len();
-    let x_max = point_matrix[0].len();
-    let mut point_list: Vec<Point> = Vec::new();
+    let row_count = point_matrix.len();
+    let column_count = point_matrix[0].len();
+    debug!("row_count {:?} column_count {:?}", row_count, column_count);
 
-    for x in 0..x_max {
-        for y in 0..y_max {
-            if point_matrix[y][x] == '#' {
-                point_list.push(Point(x, y));
+    let mut point_list: Vec<Point> = Vec::new();
+    for x in 0..row_count {
+        for y in 0..column_count {
+            if point_matrix[x][y] == '#' {
+                point_list.push(Point(y, x));
             }
         }
     }
@@ -77,65 +122,47 @@ fn get_point(point_matrix: &Vec<Vec<char>>) -> Vec<Point> {
 }
 
 fn fill(point_list: &Vec<Point>, point_matrix: &mut Vec<Vec<char>>) {
-    debug!("point_matrix - before fill {:?}", point_matrix);
+    debug!("before fill {:?}", point_matrix);
     for point in point_list {
-        point_matrix[point.1][point.0] = '#';
+        point_matrix[point.0][point.1] = '#';
     }
-    debug!("point_matrix - after fill {:?}", point_matrix);
+    debug!("after fill {:?}", point_matrix);
 }
 
-fn get_dot_count(x_max: usize, y_max: usize, point_matrix: &Vec<Vec<char>>) -> usize {
-    let mut count: usize = 0;
-    for x in 0..x_max {
-        for y in 0..y_max {
-            if point_matrix[y][x] == '#' {
-                count += 1;
-            }
-        }
-    }
+fn fold(min_point: &mut Point, fold: Fold, point_matrix: &mut Vec<Vec<char>>) {
+    let row_count = point_matrix.len();
+    let column_count = point_matrix[0].len();
+    debug!("row_count {:?} column_count {:?}", row_count, column_count);
 
-    count
-}
+    debug!("min_point {:?}", min_point);
+    let point_list: Vec<Point> = get_point(&point_matrix);
+    debug!("point_list {:?}", point_list);
 
-fn fold(fold_list: &Vec<Fold>, point_matrix: &mut Vec<Vec<char>>) {
-    let mut y_max = point_matrix.len();
-    let mut x_max = point_matrix[0].len();
-    let mut fold_count = 0;
-
-    for fold in fold_list {
-        debug!("point_matrix before fold {:?}", point_matrix);
-        let point_list: Vec<Point> = get_point(point_matrix);
-        debug!("point_list {:?}", point_list);
-
+    let mut fold_point_lit: Vec<Point> = Vec::new();
+    if fold.0 == 'y' {
+        min_point.1 = fold.1;
         for point in &point_list {
-            if fold.0 == 'y' {
-                y_max = fold.1;
-                if point.1 > fold.1 {
-                    // debug!("before {:?} {:?}", point.0, point.1);
-                    let new_y = (fold.1 - (point.1 % fold.1)) % fold.1;
-                    // debug!("after {:?} {:?}", point.0, new_y);
-                    point_matrix[new_y][point.0] = '#';
-                }
-            } else {
-                x_max = fold.1;
-                if point.0 > fold.1 {
-                    // debug!("before {:?} {:?}", point.0, point.1);
-                    let new_x = (fold.1 - (point.0 % fold.1)) % fold.1;
-                    // debug!("after {:?} {:?}", new_x, point.1);
-                    point_matrix[point.1][new_x] = '#';
-                }
+            if point.1 > fold.1 && point.0 < row_count {
+                debug!("y before {:?} {:?}", point.0, point.1);
+                let new_y = (fold.1 - (point.1 % fold.1)) % fold.1;
+                debug!(" - y after {:?} {:?}", point.0, new_y);
+                fold_point_lit.push(Point(point.0, new_y));
             }
         }
-
-        fold_count += 1;
-        let dot_count = get_dot_count(x_max, y_max, point_matrix);
-        debug!(
-            "point_matrix after fold {} time {:?} --{:?}--",
-            fold_count, point_matrix, dot_count
-        );
-
-        if fold_count == 1 {
-            info!("part 1: {}", dot_count);
+    } else {
+        min_point.0 = fold.1;
+        for point in &point_list {
+            if point.0 > fold.1 && point.1 < column_count {
+                debug!("x before {:?} {:?}", point.0, point.1);
+                let new_x = (fold.1 - (point.0 % fold.1)) % fold.1;
+                debug!(" - x after {:?} {:?}", new_x, point.1);
+                fold_point_lit.push(Point(new_x, point.1));
+            }
         }
+    }
+
+    debug!("fold_point_lit {:?}", fold_point_lit);
+    for fold_point in fold_point_lit {
+        point_matrix[fold_point.1][fold_point.0] = '#'
     }
 }
