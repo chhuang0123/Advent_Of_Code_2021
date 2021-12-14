81aea109eaaae045d8af5ce3364dc4bc9a71d4fa
Eric C Huang <eric_c_huang@trendmicro.com>
2021-12-15 07:17:57 +0800

    day14 part 2

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -31,13 +31,63 @@ fn main() {
 
     debug!("mapping {:?}", mapping);
     debug!("template {:?}", template);
-    part1(&mapping, &template);
+    part1(&mapping, &template, 10);
+    part2(&mapping, &template, 40);
 }
 
-fn part1(mapping: &BTreeMap<(char, char), char>, template: &Vec<char>) {
+fn part2(mapping: &BTreeMap<(char, char), char>, template: &Vec<char>, stop: usize) {
+    let mut steps: BTreeMap<(char, char), usize> = BTreeMap::new();
+    for (i, _) in template.iter().enumerate() {
+        let pair = if i == template.len() - 1 {
+            (template[i], ' ')
+        } else {
+            (template[i], template[i + 1])
+        };
+        steps.insert(pair, 1);
+    }
+    debug!("{:?}", steps);
+
+    for i in 0..stop {
+        let mut new_steps: BTreeMap<(char, char), usize> = BTreeMap::new();
+        for (pair, value) in steps.iter() {
+            if mapping.contains_key(pair) {
+                let new_char = mapping[pair];
+                debug!("new_step - {:?} {:?}", pair, new_char);
+                debug!("new_step - 1 {:?}", (pair.0, new_char));
+                debug!("new_step - 2 {:?}", (new_char, pair.1));
+                *new_steps.entry((pair.0, new_char)).or_insert(0) += *value;
+                *new_steps.entry((new_char, pair.1)).or_insert(0) += *value;
+            } else {
+                new_steps.insert(*pair, *value);
+            }
+        }
+
+        debug!(
+            "new_steps {:?} ({:?}) - {:?}",
+            new_steps,
+            new_steps.len(),
+            new_steps.values().cloned().sum::<usize>()
+        );
+        steps = new_steps.clone();
+
+        if i == stop - 1 {
+            let mut final_result: BTreeMap<char, usize> = BTreeMap::new();
+            for (pair, value) in steps.iter() {
+                *final_result.entry((*pair).0).or_insert(0) += value;
+            }
+
+            let max = final_result.values().max().unwrap();
+            let min = final_result.values().min().unwrap();
+            info!("{:?}", final_result);
+            info!("part 2: max {:?} - min {:?} = {:?}", max, min, max - min);
+        }
+    }
+}
+
+fn part1(mapping: &BTreeMap<(char, char), char>, template: &Vec<char>, stop: usize) {
     let mut template = template.clone();
 
-    for i in 0..10 {
+    for i in 0..stop {
         let result = replacement(&mapping, &template);
         debug!("After step {:?}: {:?}", i + 1, String::from_iter(&result));
         template = result;
