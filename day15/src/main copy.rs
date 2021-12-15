76f5af3af8ad362b3ed78ae64a747b9baa749afe
Eric C Huang <eric_c_huang@trendmicro.com>
2021-12-16 07:47:44 +0800

    day15 part 2

--- day15/src/main copy.rs
+++ day15/src/main copy.rs
@@ -23,8 +23,8 @@ fn main() {
     let row_count = risk.len();
     let column_count = risk[0].len();
     debug!("row_count {:?} column_count {:?}", row_count, column_count);
-
     let result = get_lowest_total_risk(row_count, column_count, &risk);
+    info!("risk: {:?}", risk);
     info!("part 1: {:?}", result);
 
     let mut five_risk: Vec<Vec<usize>> = vec![vec![0; column_count * 5]; row_count * 5];
@@ -38,8 +38,8 @@ fn main() {
             five_risk[i][j] = result;
         }
     }
-    debug!("{:?}", five_risk);
 
+    info!("five_risk: {:?}", five_risk);
     let result = get_lowest_total_risk(row_count * 5, column_count * 5, &five_risk);
     info!("part 2: {:?}", result);
 }
@@ -47,14 +47,14 @@ fn main() {
 fn get_lowest_total_risk(m: usize, n: usize, risk: &Vec<Vec<usize>>) -> usize {
     let row_count = risk.len();
     let column_count = risk[0].len();
-    let mut total_risk: Vec<Vec<usize>> = vec![vec![0_usize; column_count]; row_count];
+    let mut total_risk: Vec<Vec<usize>> = vec![vec![0; column_count]; row_count];
 
     for i in 1..row_count {
-        total_risk[0][i] = total_risk[0][i - 1] + risk[0][i];
+        total_risk[0][i] = total_risk[0][i - 1] + risk[0][i] as usize;
     }
 
     for j in 1..row_count {
-        total_risk[j][0] = total_risk[j - 1][0] + risk[j][0];
+        total_risk[j][0] = total_risk[j - 1][0] + risk[j][0] as usize;
     }
 
     for i in 1..row_count {
@@ -63,8 +63,15 @@ fn get_lowest_total_risk(m: usize, n: usize, risk: &Vec<Vec<usize>>) -> usize {
                 .iter()
                 .min()
                 .unwrap()
-                + risk[j][i];
+                + risk[j][i] as usize;
         }
+    }
+
+    for i in 0..row_count {
+        for j in 0..column_count {
+            print!("{:02} ", total_risk[i][j]);
+        }
+        println!();
     }
 
     total_risk[n - 1][m - 1]
